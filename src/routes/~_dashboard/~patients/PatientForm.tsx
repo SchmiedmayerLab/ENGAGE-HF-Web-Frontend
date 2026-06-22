23f6d5a2f70f8bf5fb0b4d8e0637545c22886f74
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2026-06-22 23:06:03 +0200

    Add permanent invitation checkmark

--- src/routes/~_dashboard/~patients/PatientForm.tsx
+++ src/routes/~_dashboard/~patients/PatientForm.tsx
@@ -43,6 +43,7 @@ export const getPatientFormSchema = (isEmailRequired: boolean) =>
     clinician: z.string().min(1, "Clinician is required"),
     dateOfBirth: z.date().optional(),
     selfManaged: z.boolean(),
+    permanent: z.boolean().nullable(),
     providerName: z.preprocess(
       (value: string | null | undefined) =>
         value === "" ? null : String(value),
@@ -106,6 +107,7 @@ export const PatientForm = ({
         user?.dateOfBirth ? parseDateOfBirth(user.dateOfBirth) : undefined,
       providerName: user?.providerName ?? "",
       selfManaged: user?.selfManaged ?? false,
+      permanent: null,
     },
   });
 
@@ -204,6 +206,29 @@ export const PatientForm = ({
                   />
                 </SideLabel>
                 <Tooltip tooltip="This feature allows patients to enter their own medication and laboratory value updates.">
+                  <InfoButton />
+                </Tooltip>
+              </div>
+            );
+          }}
+        />
+      )}
+      {!isEdit && (
+        <Field
+          control={form.control}
+          name="permanent"
+          render={({ field }) => {
+            const { value, onChange, ...restField } = field;
+            return (
+              <div className="flex items-center gap-2">
+                <SideLabel label="Is permanent invitation">
+                  <Checkbox
+                    checked={value ?? false}
+                    onCheckedChange={onChange}
+                    {...restField}
+                  />
+                </SideLabel>
+                <Tooltip tooltip="Permanent invitations are not deleted after the first enrollment, so the invitation code can be reused to enroll multiple patients.">
                   <InfoButton />
                 </Tooltip>
               </div>
